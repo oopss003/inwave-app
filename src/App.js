88ecb76abb9e421e7f5d459446a5a7e5d9f73fb8
oopss003 <jhpark8271@gmail.com>
2025-04-21 20:35:56 +0900

    🎉 최초 업로드

---
+++ src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div style={{
+      backgroundColor: '#111',
+      color: '#00ffcc',
+      fontSize: '48px',
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      hi.inwave
+    </div>
+  );
+}
+
+export default App;
